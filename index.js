f7878176724e35afaa264ec889b3136ead7c0f02
Nachopozzolo <83032109+Nachopozzolo@users.noreply.github.com>
2022-11-15 16:52:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const miNombre = "Nacho";
+const edad = 22;
+const soyDesarrollador = true;
+const nacimiento = new Date("september 16 2000");
+
+let libroFav: 
+autor: "Roy berocay",
+anio: 2010,
+imprenta: "Sdc";
+
+
+console.log(libroFav.imprenta);
